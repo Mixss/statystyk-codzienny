0fd2edc1ca34e78e8b2c93ddee74d41cc2a28123
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2022-03-31 09:35:52 +0200

    added temperatures to forecast image

--- forecast_image.py
+++ forecast_image.py
@@ -1,6 +1,7 @@
 import matplotlib as mp
 import numpy as np
 import cv2 as cv
+from PIL import ImageFont, ImageDraw, Image
 
 SPACE_BETWEEN_LINES = 326
 LINE_OFFSET_LEFT = 8
@@ -8,8 +9,9 @@ LINE_OFFSET_LEFT = 8
 LINE_COLOR = (131, 128, 126)
 
 TEXT_TOP_OFFSET = 30
+TEMP_TOP_OFFSET = 50
 
-HOUR_VERT_OFFSET = 250
+HOUR_VERT_OFFSET = 400
 
 # get dictionary
 # hour: , temperature, icon(description), wind speed, wind dir
@@ -29,6 +31,40 @@ def draw_lines():
         cv.line(image, (x_coord, 5), (x_coord, image_height - 5), LINE_COLOR, 1)
 
 
+def draw_temperatures(img, color, text_scale):
+    temperatures = [3, 7, 18, 9, -3, 21]
+
+    font = cv.FONT_HERSHEY_DUPLEX
+
+    lines = create_lines()[:-1]
+
+    lines += lines
+
+    image_pil = Image.fromarray(img)
+    draw = ImageDraw.Draw(image_pil)
+
+    for line_x, temp in zip(lines, temperatures):
+        text_size = cv.getTextSize(str(temp), font, text_scale, 2)[0]
+
+        center_x = line_x + SPACE_BETWEEN_LINES / 2
+
+        unicode_font = ImageFont.truetype("./fonts/IBMPlexSansArabic-Light.ttf", 60)
+
+        text_x = center_x - unicode_font.getsize(str(temp) + "°C")[0] / 2
+
+        if temperatures.index(temp) < 3:
+            draw.text((int(text_x), TEMP_TOP_OFFSET), str(temp) + "°C", font=unicode_font, fill=color)
+            #cv.putText(image, str(temp) + "°C", (int(text_x), TEMP_TOP_OFFSET), font, text_scale, color, 1, bottomLeftOrigin=False, lineType=cv.LINE_AA)
+        else:
+            draw.text((int(text_x), TEMP_TOP_OFFSET + HOUR_VERT_OFFSET), str(temp) + "°C", font=unicode_font, fill=color)
+            #cv.putText(image, str(temp) + "°C", (int(text_x), TEMP_TOP_OFFSET + HOUR_VERT_OFFSET), font, text_scale, color, 1, bottomLeftOrigin=False, lineType=cv.LINE_AA)
+
+    return np.array(image_pil)
+
+
+def draw_icons():
+    
+
 def draw_text(color, text_scale):
     hours = ["08:00", "10:00", "12:00", "14:00", "16:00", "18:00"]
 
@@ -37,8 +73,6 @@ def draw_text(color, text_scale):
     lines = create_lines()[:-1]
 
     lines += lines
-
-    print(lines)
 
     for line_x, hour in zip(lines, hours):
         # center text
@@ -55,7 +89,7 @@ def draw_text(color, text_scale):
 
 
 image_width = 1000
-image_height = 500
+image_height = 800
 channels = 3
 # color = (59, 53, 50)  # discord darker color
 color = (63, 57, 54)
@@ -64,6 +98,7 @@ image = np.full((image_height, image_width, channels), color, dtype=np.uint8)
 
 draw_lines()
 draw_text((191, 188, 186), 0.9)
+image = draw_temperatures(image, (221, 218, 216), 1.2)
 
 cv.imshow('image', image)
 cv.waitKey(0)
