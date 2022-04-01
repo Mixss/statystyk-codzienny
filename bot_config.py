835c155bcabf81d0705a37018bbb3bcb3b2db50c
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2022-04-01 15:39:59 +0200

    added deadlines command

--- bot_config.py
+++ bot_config.py
@@ -57,20 +57,23 @@ def get_daily_stats_message():
               f" \n" \
               f"Imieniny: :couple: *{get_today_names()}* \n" \
               f" \n" \
-              f" :white_sun_cloud: **Prognoza pogody**\n" \
-              f" :thermometer: Temperatura:\n" \
-              f" > :snowflake: min: **{minTemp} °C**\n" \
-              f" > :fire: max: **{maxTemp} °C**\n" \
-              f" \n:white_sun_small_cloud: Spodziewana pogoda: \n" \
-              f"*{forecastInfo}* \n" \
-              f"*Prawdopodobieństwo deszczu:*  **{raining}%** \n\n" \
               f":mask: Jakość powietrza: *{air}* \n" \
               f":sunglasses: Index UV: **{uvidex}** ({uvdanger}) \n " \
               f" \n" \
-              f":moneybag: Finanse: \n" \
-              f":euro: 1 EUR = **{euro}** PLN\n" \
-              f":dollar: 1 USD = **{usd}** PLN\n" \
+              f" :white_sun_cloud: **Prognoza pogody**\n" \
               f""
+
+    # f":moneybag: Finanse: \n" \
+    # f":euro: 1 EUR = **{euro}** PLN\n" \
+    # f":dollar: 1 USD = **{usd}** PLN\n" \
+
+    # f" :white_sun_cloud: **Prognoza pogody**\n" \
+    # f" :thermometer: Temperatura:\n" \
+    # f" > :snowflake: min: **{minTemp} °C**\n" \
+    # f" > :fire: max: **{maxTemp} °C**\n" \
+    # f" \n:white_sun_small_cloud: Spodziewana pogoda: \n" \
+    # f"*{forecastInfo}* \n" \
+    # f"*Prawdopodobieństwo deszczu:*  **{raining}%** \n\n" \
 
     return message
 
@@ -83,5 +86,26 @@ def get_current_weather_message():
               f":dash: Prędkość wiatru: **{wind_s}** km/h \n" \
               f":cloud_rain: Opady: **{fall}** mm/h \n" \
               f":clock: Ciśnienie: **{press}** hPa"
+
+    return message
+
+
+def get_deadlines_message(number_of_deadlines):
+    courses, dates, descriptions = get_deadlines()
+
+    text = ""
+    if number_of_deadlines == 1:
+        text = "najbliższy ważny termin"
+    elif number_of_deadlines == 3 or number_of_deadlines == 4:
+        text = "najbliższe ważne terminy"
+    else:
+        text = "najbliższych ważnych terminów"
+
+    message = f':flushed: **{number_of_deadlines} {text}**\n\n'
+
+    for course, date, desc in zip(courses[:number_of_deadlines], dates[:number_of_deadlines],
+                                  descriptions[:number_of_deadlines]):
+        message += f'**{course}**  -  {date}\n'
+        message += f'*`{desc}`*\n\n'
 
     return message
